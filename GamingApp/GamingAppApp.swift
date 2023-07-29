9a012f4c43a88f2a0490d4d19e308a3ff2e12acf
Gytis Ptašinskas <g.ptasinskas@gmail.com>
2023-07-29 12:34:02 +0300

    Initial Commit

---
+++ GamingApp/GamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamingAppApp.swift
+//  GamingApp
+//
+//  Created by Gytis Ptašinskas on 2023-07-29.
+//
+
+import SwiftUI
+
+@main
+struct GamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
